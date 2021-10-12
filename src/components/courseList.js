44c2f8ae43a5e31e27acc58a14572c5a44fe9d95
chaalia <rejeb.chaalia@gmail.com>
2021-10-12 22:44:25 +0100

    add update course

--- src/components/courseList.js
+++ src/components/courseList.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
 
 class CourseList extends Component{
+    state = {
+        isEdit : false
+    }
 
     // render course item
     renderCourse = () => {
+
         return(
           <ul> 
               <li>
                 <span>{this.props.details.name}</span>
+                <button onClick={() => this.toggleState()}>Edit Course</button>
                 <button onClick={() => {this.props.deleteCourse(this.props.index)}}>Delete course </button>
              </li>
              </ul>
         )
     }
+    // render update form
+     renderUpdateForm = () => {
+        return (
+            <form>
+                <input type="text"/>
+                <button>Update form</button>
+            </form>
+            
+        )
+    }
+
+    //toggle state
+     toggleState = () => {
+        let {isEdit} = this.state
+        this.setState(
+            {isEdit: !isEdit}
+        )
+     } 
 
     render() { 
+        let {isEdit} = this.state
     return(
         <React.Fragment>
                 {/* <button> Update Course</button> */}
-                {this.renderCourse()}
+                {isEdit ? this.renderUpdateForm() : this.renderCourse()}
             
         </React.Fragment>
     );
